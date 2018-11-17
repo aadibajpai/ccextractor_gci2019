12b527a600a22a12572046fd0a48d5374af3e72b
John Chew <kengweechew@gmail.com>
2018-11-17 23:32:22 +0800

    Initial Commit - v1.0 Site Test
    Home Page v1.0

---
+++ js/str.js
@@ -0,0 +1,21 @@
+window.sr = new ScrollReveal({reset: true});
+
+var fadeFromTop = {
+  delay: 100,
+  distance: '20px',
+  origin: 'top',
+}
+
+var fadeFromBottom = {
+  delay: 100,
+  distance: '25px',
+  origin: 'bottom',
+}
+
+ScrollReveal().reveal(
+  '#logo_stylized, #sectHead, #sectPara, .sectLinks, #sectSubHead', fadeFromTop
+);
+
+ScrollReveal().reveal(
+  '.svgItem', fadeFromBottom
+);
