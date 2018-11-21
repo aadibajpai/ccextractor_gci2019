9d254a3363f00f471b3cff2803ed3a6e9f7c1d17
John Chew <kengweechew@gmail.com>
2018-11-21 22:23:41 +0800

    Design Update : Link Hover Animation

--- js/str.js
+++ js/str.js
@@ -1,21 +1,21 @@
 window.sr = new ScrollReveal();
 
 var fadeFromTop = {
-  delay: 100,
+  delay: 250,
   distance: '20px',
   origin: 'top',
 }
 
 var fadeFromBottom = {
-  delay: 100,
+  delay: 250,
   distance: '25px',
   origin: 'bottom',
 }
 
 ScrollReveal().reveal(
-  '#logo_stylized, .sectHead, .sectPara, .sectLinks, .sectSubHead', fadeFromTop
+  '#logo_stylized, #gci_logo_stylized, #gsoc_logo_stylized, h1, h2, h3, h4, h5, h6, p, .btn-info, .sectHead, .sectPara, .sectLinks, .sectSubHead', fadeFromTop
 );
 
 ScrollReveal().reveal(
-  '.svgItem', fadeFromBottom
+  '.mainBackgroundItem, .sect2BackgroundItem, .sect3BackgroundItem', fadeFromBottom
 );
